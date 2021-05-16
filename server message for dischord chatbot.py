c6eb6b14d31591e5319dde774aaad8b1d6a30cf3
RajrupDasid <rajrupdasid@outlook.com>
2021-05-16 21:32:47 +0530

    My full python learning

---
+++ server message for dischord chatbot.py
@@ -0,0 +1,17 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Hello. I am alive!"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
